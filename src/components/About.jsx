10314638c2dfc2bf0ff06ef188e32a1650622638
SebastianSkowron <sebastian.skowron.dev@gmail.com>
2022-11-12 15:33:46 +0100

    kinda good, going to modify and repair and add least things, and lines of code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,21 +29,21 @@ export const About = () => {
           <div className="flex flex-col items-end px-3">
             <img
               className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
-              src="/asset.png"
-              alt=""
+              src="/about-img-1.jpg"
+              alt="about-img-1-cucumbers-and-tomatoes"
             />
 
             <img
               className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
-              src="/asset.png"
-              alt=""
+              src="/about-img-2.jpg"
+              alt="about-img-2-cabbages"
             />
           </div>
           <div className="px-3">
             <img
               className="object-cover w-40 h-40 rounded-lg shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
-              src="/asset.png"
-              alt=""
+              src="/about-img-3.jpg"
+              alt="about-img-3-tomatoes-in-greenhouse"
             />
           </div>
         </div>
